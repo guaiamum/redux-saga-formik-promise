1aa21a9e42fe77c8146ccdda2e41767ca62e2326
Cainã Brazil <cbr.guaiamum@gmail.com>
2021-08-21 11:32:10 +0100

    promisified loading state (lost serializability)

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,13 +1,14 @@
-import { call, put, takeLeading } from "redux-saga/effects";
+import { call, takeLeading } from "redux-saga/effects";
 
 import { apiConnection } from "../services/apiConnection";
 
 function* postForm(action) {
   try {
     const { teams } = yield call(apiConnection, "quick", action.data);
-    yield put({ type: "POST_FORM_SUCCESS", teams });
+
+    action.res(teams);
   } catch (e) {
-    yield put({ type: "POST_FORM_FAILED", error: e.message });
+    action.rej();
   }
 }
 
